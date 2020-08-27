51e6707a2b6eef8d879e99c78285b58906be72b7
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-08-27 18:05:12 -0300

    :zap: Adiciona módulo de dependência do Angular Material para o componente nav

--- frontend/src/app/angular-material.module.ts
+++ frontend/src/app/angular-material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [MatListModule, MatSidenavModule, MatToolbarModule],
+  exports: [MatListModule, MatSidenavModule, MatToolbarModule],
 })
 export class AngularMaterialModule {}
